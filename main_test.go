23e4656bfd8d1174649b30dcefd5ece009db2602
molon <dudl@qq.com>
2018-11-25 21:10:47 +0800

    fix test

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestHelloWorld(t *testing.T) {
-	if HelloWorld() != "hello world" {
+	if HelloWorld() != "hello world！" {
 		t.Errorf("got %s expected %s", HelloWorld(), "hello world")
 	}
 }
